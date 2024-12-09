f22c327d9d1b7a9852d829a48389818e8462c1f3
Muhammad Ameer Hamza <129995036+nexusameer@users.noreply.github.com>
2024-12-09 21:57:34 +0500

    final paper

---
+++ Final Paper/Final Paper.cpp
@@ -0,0 +1,9 @@
+// Final Paper.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!\n";
+}
